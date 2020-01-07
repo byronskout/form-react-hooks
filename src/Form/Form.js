dc56e233725dab2b027cfd43aa231395d738ad01
byronskout <vyskoutaris@hotmail.com>
2020-01-07 17:01:18 +0000

    handleItemChange for Item fields

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -19,24 +19,24 @@ const Form = () => {
 		setItemState([ ...itemState, { ...blankItem } ]);
 	};
 
+	const handleItemChange = (e) => {
+		const updatedItems = [ ...itemState ];
+		updatedItems[e.target.dataset.idx][e.target.className] = e.target.value;
+		setItemState(updatedItems);
+	};
+
 	return (
 		<form>
 			<label htmlFor="owner">Owner</label>
-            <input 
-            type="text" 
-            name="owner" 
-            id="owner"
-            value={ownerState.owner}
-            onChange={handleOwnerChange} 
-            />
+			<input type="text" name="owner" id="owner" value={ownerState.owner} onChange={handleOwnerChange} />
 			<label htmlFor="description">Description</label>
-            <input 
-            type="text" 
-            name="description" 
-            id="description" 
-            value={ownerState.owner}
-            onChange={handleOwnerChange}
-            />
+			<input
+				type="text"
+				name="description"
+				id="description"
+				value={ownerState.owner}
+				onChange={handleOwnerChange}
+			/>
 			<input type="button" value="Add Item" onClick={addItem} />
 			{itemState.map((val, idx) => {
 				const itemId = `name-${idx}`;
@@ -44,7 +44,14 @@ const Form = () => {
 				return (
 					<div key={`item-${idx}`}>
 						<label htmlFor={itemId}>{`Item #${idx + 1}`}</label>
-						<input type="text" name={itemId} data-idx={idx} id={itemId} className="name" />
+						<input
+							type="text"
+							name={itemId}
+							data-idx={idx}
+							id={itemId}
+							className="name"
+							onChange={handleItemChange}
+						/>
 						<label htmlFor={descriptionId}>Description</label>
 						<input
 							type="text"
@@ -52,6 +59,7 @@ const Form = () => {
 							data-idx={idx}
 							id={descriptionId}
 							className="description"
+							onChange={handleItemChange}
 						/>
 					</div>
 				);
