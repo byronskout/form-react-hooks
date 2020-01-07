11f3d5435028daa9890ee28dabe50b64000f67c9
byronskout <vyskoutaris@hotmail.com>
2020-01-07 16:32:35 +0000

    Added basic html for form

---
+++ src/Form/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form = () => {    
+  return (        
+    <form>            
+      <label htmlFor="owner">Owner</label>   
+      <input type="text" name="owner" id="owner" /> 
+      <label htmlFor="description">Description</label> 
+      <input type="text" name="description" id="description" />
+      <input type="button" value="Add New Dog" />            
+      <input type="submit" value="Submit" />        
+    </form>   
+  );
+}; 
+export default Form;
