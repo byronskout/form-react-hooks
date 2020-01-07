41c6b0a3a9b03c6bf07e04e90b3aea971e76de8f
byronskout <vyskoutaris@hotmail.com>
2020-01-07 16:43:02 +0000

    Form updated + useState

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Form = () => {    
-  return (        
-    <form>            
-      <label htmlFor="owner">Owner</label>   
-      <input type="text" name="owner" id="owner" /> 
-      <label htmlFor="description">Description</label> 
-      <input type="text" name="description" id="description" />
-      <input type="button" value="Add New Dog" />            
-      <input type="submit" value="Submit" />        
-    </form>   
-  );
-}; 
+const Form = () => {
+    const blankItem = { name: '', description: '' };
+    const [itemState, setItemState] = useState([
+      {...blankItem}
+    ]);
+
+    const addItem = () => {
+        setItemState([...itemState, {...blankItem}]);
+      };
+
+	return (
+		<form>
+			<label htmlFor="owner">Owner</label>
+			<input type="text" name="owner" id="owner" />
+			<label htmlFor="description">Description</label>
+			<input type="text" name="description" id="description" />
+            <input type="button" value="Add Item"
+            onClick={addItem}
+            />
+			{itemState.map((val, idx) => {
+				const itemId = `name-${idx}`;
+				const descriptionId = `description-${idx}`;
+				return (
+					<div key={`item-${idx}`}>
+						<label htmlFor={itemId}>{`Item #${idx + 1}`}</label>
+						<input type="text" name={itemId} data-idx={idx} id={itemId} className="name" />
+						<label htmlFor={descriptionId}>Description</label>
+						<input
+							type="text"
+							name={descriptionId}
+							data-idx={idx}
+							id={descriptionId}
+							className="description"
+						/>
+					</div>
+				);
+			})}
+			<input type="submit" value="Submit" />
+		</form>
+	);
+};
 export default Form;
